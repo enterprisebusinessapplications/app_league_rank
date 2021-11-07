8eacc560843a1c167fbc174d242ecc2e1b91806b
Praise Tompane <praisetompane@gmail.com>
2021-11-07 12:56:38 +0200

    a more robust version of the CLI entry point

--- main/src/main/scala/com/spandigital/leaguerank/cli/Main.scala
+++ main/src/main/scala/com/spandigital/leaguerank/cli/Main.scala
@@ -3,16 +3,24 @@ package com.spandigital.leaguerank.cli
 import com.spandigital.leaguerank.gateway.MatchResultsFileReader
 import com.spandigital.leaguerank.core.LeagueRankingCaculator
 object Main extends App {
-  if (args.length == 0) 
-    println("Please provide a filename.")
-  else if (args.length > 1)
-    println("Please provide one filename.")
-  else {
-    val matchResultsFileName = args(0)
-    println("Commencing league rank calculation...")
-    //TODO what about exception handling here, leave it in the reader?
-    val matchReults = MatchResultsFileReader.read(matchResultsFileName)
-    val leaguaeRankintTable = LeagueRankingCaculator.calculate(matchReults)
-    //format and print results in desired format
+  try {
+    if (args.length == 0)
+      println("Please provide a filename.")
+    else if (args.length > 1)
+      println("Please provide one filename.")
+    else {
+      val matchResultsFileName = args(0)
+      println("Commencing league rank calculation...")
+      val matchResults = MatchResultsFileReader.read(matchResultsFileName)
+      val leagueTable = LeagueRankingCaculator.calculate(matchResults)
+      print(leagueTable)
+      println("Done")
+    }
+  } catch {
+    case e: Exception =>
+      println(
+        "failed during the execution of ranking teams. Please refer to stack trace below"
+      )
+      e.printStackTrace()
   }
 }

55812edc7e31d1428a34491e708d673f57b3f318
Praise Tompane <praisetompane@gmail.com>
2021-11-10 23:30:47 +0200

    unit test TeamScore object and some refactoring on it

--- model/src/main/scala/com/spandigital/leaguerank/model/TeamScore.scala
+++ model/src/main/scala/com/spandigital/leaguerank/model/TeamScore.scala
@@ -3,9 +3,9 @@ package com.spandigital.leaguerank.model
 case class TeamScore(name: String, score: Int)
 
 object TeamScore {
-  val alphaNumbericPattern = "([a-zA-Z- ]*) ([0-9]*)".r
+  val alphaNumbericPattern = "([a-zA-Z ]*) ([0-9]*)".r
   def apply(teamMatchScore: String): TeamScore = teamMatchScore match {
-    case alphaNumbericPattern(team, score) => TeamScore(team, score.toInt)
-    case _ => throw new IllegalArgumentException("invalid team result format")
+    case alphaNumbericPattern(name, score) => TeamScore(name, score.toInt)
+    case _ => throw new IllegalArgumentException("invalid team match score format")
   }
 }
--- model/src/test/scala/com/spandigital/leaguerank/model/TeamScoreUnitTests.scala
+++ model/src/test/scala/com/spandigital/leaguerank/model/TeamScoreUnitTests.scala
@@ -8,17 +8,28 @@ class TeamScoreUnitTests extends AnyFunSuite {
 
   test("can handle single word team name and score") {
     val teamMatchScore = "Lions 3"
-    val teamResult = TeamScore(teamMatchScore)
+    val teamScore = TeamScore(teamMatchScore)
     assert(
-      TeamScoreDefaultObject.copy(name = "Lions", score = 3) == teamResult
+      TeamScoreDefaultObject.copy(name = "Lions", score = 3) == teamScore
+    )
+  }
+
+  test("can handle team name with 2 words and score") {
+    val teamMatchScore = "FC Awesome 5"
+    val teamScore = TeamScore(teamMatchScore)
+    assert(
+      TeamScoreDefaultObject.copy(name = "FC Awesome", score = 5) == teamScore
     )
   }
 
   test("can handle team name with more than 2 words and score") {
-    val teamMatchScore = "FC Awesome 5"
-    val teamResult = TeamScore(teamMatchScore)
+    val teamMatchScore = "FC Awesome Players 5"
+    val teamScore = TeamScore(teamMatchScore)
     assert(
-      TeamScoreDefaultObject.copy(name = "FC Awesome", score = 5) == teamResult
+      TeamScoreDefaultObject.copy(
+        name = "FC Awesome Players",
+        score = 5
+      ) == teamScore
     )
   }
 }
